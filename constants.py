ea031e3f363bde6a6352586c852a1a61db302961
Pc Repuestos 01 <repuestos01@REPUESTOS.LOCAL>
2026-04-01 09:35:05 -0300

    Fix base_dir

--- constants.py
+++ constants.py
@@ -1,23 +1,10 @@
-import sys
 from enum import auto, StrEnum
 from pathlib import Path
 from strenum import UppercaseStrEnum
-
-def obtener_ruta_base():
-    if getattr(sys, 'frozen', False):
-        # Si es un EXE, sacamos la ruta de donde está el archivo .exe físico
-        return Path(sys.executable).resolve().parent
-    else:
-        # Si es el script de Python normal
-        return Path(__file__).resolve().parent
     
-BASE_DIR = obtener_ruta_base()
+BASE_DIR = Path(__file__).resolve().parent
 
-OFICINA_PATH = BASE_DIR / "pedidos"
-OFICINA_XLSX_PATH = BASE_DIR / "pedidos_separados"
 CODIGOS_PATH = BASE_DIR / "codigos"
-# print(f"DEBUG: Buscando archivos en -> {BASE_DIR}")
-
 COLUMNAS = {"Articulo    Cod Prov                     Descripcion                                 Cantidad   Prec Unit     S-Total":"Columnas"}
 
 class TipoPedidoEnum(UppercaseStrEnum):
